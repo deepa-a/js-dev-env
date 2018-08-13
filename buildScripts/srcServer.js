bd19ed9a79ead6515904f7e7548fd7b07f661a18
Deepa Aravindan <Deepa.Aravindan@unico.com.au>
2018-08-13 14:16:10 +1000

    second commit

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id":1, "firstName": "Bob", "lastName":"Smith","email":"bob@bob.com"},
+    {"id":2, "firstName": "Bobby", "lastName":"Jain","email":"bob@gmail.com"},
+    {"id":3, "firstName": "Jack", "lastName":"Stller","email":"bob@hotmail.com"},
+  ]);
+});
+
+
 app.listen(port, function(err) {
   if(err){
     console.log(err); // eslint-disable-line no-console
